b2497bf5e5e79a3fb530453571ab4f356a86c3cf
superonetrabalhoone <96762353+superonetrabalhoone@users.noreply.github.com>
2022-11-08 17:48:48 -0300

    Changed

---
+++ precache-manifest.856ebd1fffbe873389f4b85648a25969.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "edce84fe31dc040220fc",
+    "url": "/static/css/main.02ba4f1c.chunk.css"
+  },
+  {
+    "revision": "edce84fe31dc040220fc",
+    "url": "/static/js/main.edce84fe.chunk.js"
+  },
+  {
+    "revision": "c9a75c3334f6beeaf760",
+    "url": "/static/js/1.c9a75c33.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37202d606c161ac3b64ecfcc5b476852",
+    "url": "/static/media/logoGoogle.37202d60.svg"
+  },
+  {
+    "revision": "f3418a443e7d841097c714d69ec4bcb8",
+    "url": "/static/media/favicon.f3418a44.ico"
+  },
+  {
+    "revision": "4d9b7e1b6654af4bf8849b06b5a4b21d",
+    "url": "/static/media/favicon_apple.4d9b7e1b.png"
+  },
+  {
+    "revision": "4e9b50389134edc50c4194a5d1f74d80",
+    "url": "/static/media/apple_intro.4e9b5038.png"
+  },
+  {
+    "revision": "644563f48ab5fe8e9082b64b2729b068",
+    "url": "/static/media/SFPRODISPLAYBOLD.644563f4.OTF"
+  },
+  {
+    "revision": "aaeac71d99a345145a126a8c9dd2615f",
+    "url": "/static/media/SFPRODISPLAYREGULAR.aaeac71d.OTF"
+  },
+  {
+    "revision": "871642142445fbe00b794d9d1c948c4d",
+    "url": "/index.html"
+  }
+];
